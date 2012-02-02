38661f0ce917a4fa6e1c86c203dac583ec6e202b
= <=>
2012-02-02 16:18:35 -0500

    added simple shell to git layout

--- Client.java
+++ Client.java
@@ -1,8 +1,58 @@
+import java.io.*;
+import java.net.*;
+
 /**
- *
+ * @desc This the client process that communicates
+ *       with the server.
  */
 public class Client
 {
 
+    private static IPAddress serverIP;
+    private static DatagramSocket server;
+    private static int port;
+
+    private static BufferedReader reader;
+    private static PrintWriter writer;
+
+    public Client(){
+
+	serverIP = null;
+	server = null;
+	serverPort = -1;
+
+	reader = null;
+	writer = null;
+
+    } // end constructor
+
+    public static void main(String[] args){
+
+	boolean activeConnection = true;
+
+	try {
+
+	}
+	catch (IOException e) {
+
+	}
+	catch (Exception e){
+	    e.printStackTrace();
+	}
+
+
+	while (activeConnection){
+
+	}
+
+    } // end main method
+
+    private static void ReadMsg(){
+
+    }
+
+    private static void SendMsg(){
+
+    }
 
 } // end class Client
