dc1ee42f93d18b01d3df95ef15da01ef608f8385
= <=>
2012-02-04 16:47:22 -0500

    changes to Client.java

--- Client.java
+++ Client.java
@@ -30,7 +30,7 @@ public class Client
 
     public static void main(String[] args){
 
-	DatagramPacket packet = new DatagramPacket(new byte[PACKET_SIZE]);
+	DatagramPacket packet = null;
 	boolean wait = false;
 
 	try {
