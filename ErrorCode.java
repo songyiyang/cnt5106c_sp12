9bcd557a8065449f6c1eafb7d0d072d6e22f9208
altendo <altendo@gmail.com>
2012-02-09 23:14:58 -0500

    added basic error codes to system.

--- ErrorCode.java
+++ ErrorCode.java
@@ -6,7 +6,18 @@
 public enum ErrorCode
 {
 
-    
+    FAIL_WHALE(72, "fail whale is failing, unknown error occurred"),
+    PACKET_EXPLODED(666, "packet inexplicably blew up"),
+    TIMEOUT(1973, "server's response timed out. it is wasting the "+
+                  "hours in an offhand way."),
+    RECORD_NOT_FOUND(404, "cannot find record");
 
+    private int number;
+    private String message;
+
+    ErrorCode(int _number, String _message){
+	number = _number;
+	message = _message;
+    }
 
 } // end enum ErrorCode
