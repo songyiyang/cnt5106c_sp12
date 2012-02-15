d20224259fcaf021113f5ff83b5333dde5c01354
= <=>
2012-02-15 12:28:39 -0500

    commented ServerProtocol (more comments needed) and made small change to documentation

--- ServerProtocol.java
+++ ServerProtocol.java
@@ -27,11 +27,14 @@ public class ServerProtocol extends Protocol
 
 	LinkedList<Record> matchedRecords = null;
 
+	    // Insert a record
 	if (tokens[1].equals("INSERT")){
 
 	    name = tokens[2];
 	    String[] address = tokens[3].split(":");
 
+		// Build the IPAddress based on passed parameter
+		// values
 	    if (address.length == 2){
 		port = Integer.parseInt(address[1]);
 		ipAddress = new IPAddress(address[0], port);
@@ -40,6 +43,7 @@ public class ServerProtocol extends Protocol
 		ipAddress = new IPAddress(address[0]);
 	    }
 
+		// Create the record and add it to the list
 	    Record record = new Record(name, ipAddress);
 	    Server.addRecord(record);
 
@@ -47,7 +51,7 @@ public class ServerProtocol extends Protocol
 
 	}
 
-
+	    // Delete a record
 	else if (tokens[1].equals("DELETE")){
 
 	    name = tokens[2];
@@ -55,6 +59,8 @@ public class ServerProtocol extends Protocol
 	    String ip = null;
 	    port = 0;
 
+		// Set the String IP to a non-null value if it exists.
+		// Otherwise set to null
 	    if (!address[0].equals("null")){
 		ip = address[0];
 	    }
@@ -62,6 +68,8 @@ public class ServerProtocol extends Protocol
 		ip = null;
 	    }
 
+		// If port was passed, add to IPAddress. Otherwise
+		// just add the String IP.
 	    if (address.length == 2){
 		port = Integer.parseInt(address[1]);
 		ipAddress = new IPAddress(ip, port);
@@ -70,8 +78,11 @@ public class ServerProtocol extends Protocol
 		ipAddress = new IPAddress(ip);
 	    }
 
+		// Attempt to delete the record
 	    boolean deleted = Server.deleteRecord(name, ipAddress);
 
+		// If record could be deleted for some reason,
+		// set the error code to RECORD_NOT_FOUND
 	    if (!deleted){
 		error = ErrorCode.RECORD_NOT_FOUND;
 	    }
@@ -81,14 +92,17 @@ public class ServerProtocol extends Protocol
 	}
 
 
-
+	    // Check to see if there are records to return
 	else if (tokens[1].equals("GET")){
 
 	    name = tokens[2];
 	    String address = tokens[3];
 
+		// Attempt to find records
 	    matchedRecords = Server.findRecords(name, address);
 
+		// If no records have been found, set error code
+		// to RECORD_NOT_FOUND
 	    if (matchedRecords.size() == 0){
 		error = ErrorCode.RECORD_NOT_FOUND;
 	    }
@@ -98,21 +112,34 @@ public class ServerProtocol extends Protocol
 	}
 
 
-
+	    // Process a shutdown request
 	else if (tokens[1].equals("GAMEOVER")){
+
+	    // For future project: detect if server is
+	    // busy and return the SERVER_BUSY error code
+	    // if it is. Right now it is a single-threaded
+	    // system so it will not be doing additional
+	    // processing in a thread
+
 	    cmd = ProtocolCommand.GAMEOVER;
+
 	}
 
+	    // Client sent test message
 	else if (tokens[1].equals("TEST")){
 	    cmd = ProtocolCommand.TEST;
 	}
 
+	    // If the server needs to respond, send response
+	    // to the client
 	if (cmd != null){
 	    system_msg = ProtocolCommand.createPacket(cmd, name,
 					              client, args, 1, error);
 	    send(socket, system_msg, client);
 	}
 
+	    // In the event that the server has found records for
+	    // a GET request, begin sending the records to the client
 	if (cmd == ProtocolCommand.GET && error == null){
 	    sendMatchedRecords(socket, client, matchedRecords);
 	}
@@ -122,6 +149,21 @@ public class ServerProtocol extends Protocol
     } // end method parseMessage
 
 
+    /**
+     * Send records to the client. This method is called
+     * when the client sends a GET request and the server
+     * can match records to the regex sent from the client.
+     *
+     * @param socket
+     *    The socket through which to communicate with the
+     *    client
+     * @param client
+     *    The IPAddress object representing the client.
+     * @param matchedRecords
+     *    LinkedList of records that match the user's
+     *    regex
+     *
+     */
     private static void sendMatchedRecords(DatagramSocket socket,
 			  IPAddress client, LinkedList<Record> matchedRecords){
 
@@ -202,9 +244,7 @@ public class ServerProtocol extends Protocol
 	try {
 	    addr = Protocol.receive(socket, packet);
 	}
-	catch (SocketTimeoutException e){
-
-	}
+	catch (SocketTimeoutException e){ }
 
 	return addr;
 
