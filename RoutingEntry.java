057b9b1a4cbe230184c64d014455e379649ab0b9
altendo <altendo@gmail.com>
2012-04-18 23:23:18 -0400

    added class that tracks a given entry in the routing table

---
+++ RoutingEntry.java
@@ -0,0 +1,49 @@
+/**
+ * @class RoutingEntry
+ *
+ * @desc Contains information about a routing entry
+ *       in the network.
+ */
+public class RoutingEntry
+{
+
+    private String server;
+    private String neighbor;
+    private int hopCount;
+
+	// Sentinal value that indicates the node is not reachable
+    public static int UNREACHABLE_NODE = -1;
+
+    public RoutingEntry(String _server, String _neighbor) {
+
+	server = _server;
+	neighbor = _neighbor;
+	hopCount = -1;
+
+    }
+
+    public RoutingEntry(String _server, String _neighbor, int _hopCount) {
+
+	server = _server;
+	neighbor = _neighbor;
+	hopCount = _hopCount;
+
+    }
+
+    public void updateHopCount(int _newHopCount){
+	hopCount = _newHopCount;
+    }
+
+    public String getServer(){
+	return server;
+    }
+
+    public String getNeighbor(){
+	return neighbor;
+    }
+
+    public int getHopCount(){
+	return hopCount;
+    }
+
+} // end class RoutingEntry
