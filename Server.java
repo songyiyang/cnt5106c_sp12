c4fe1a5dc2ff609830a16d41681bf974f8b4e71c
altendo <altendo@gmail.com>
2012-04-21 09:49:16 -0400

    added code to send mail to arbitrary IP

--- Server.java
+++ Server.java
@@ -250,6 +250,35 @@ public class Server
 
 
     /**
+     * Finds and returns the registered name given an IP, if it exists. 
+     *
+     * @param addr
+     *    The IP addresss on which to search.
+     *
+     * @return
+     *    The RegisteredName object if found, or null if not found.
+     */
+    public static RegisteredName findRegisteredNameIP(IPAddress addr){
+
+	RegisteredName rname = null;
+	IPAddress ip = null;
+
+	for (RegisteredName temp : registrar){
+
+	    ip = temp.getIP(); 
+
+	    if (ip.getIPAddress().equals(addr.getIPAddress())){
+		rname = temp;
+		break;
+	    } // end if temp.getIPAddress
+
+	} // end for RegisteredName
+
+	return rname;
+
+    } // end findRegisteredNameIP
+
+    /**
      * Registers a name on the server.
      *
      * @param rname
@@ -315,152 +344,19 @@ public class Server
 
     } // end sendMailToClients
 
-
-    /****************************************************
-
-
-	methods to add/delete/find records
-
-
-    *****************************************************/
-
-
     /**
-     * Add a new record to the list of records.
-     */
-    public static boolean addRecord(Record record){
-
-	boolean added = false;
-	boolean duplicateExists = false;
-
-	for (Record temp : records){
-	    if (temp.getName().equals(record.getName())){
-		duplicateExists = true;
-		break;
-	    }
-	}
-
-	if (!duplicateExists){
-	    records.addLast(record);
-	    added = true;
-	}
-
-	return added;
-
-    } // end method addRecord
-
-    /**
-     * Delete the specified record.
+     * Send mail to a given IP address.
      *
-     * @param name
-     *    The name of the record to delete.
-     * @param ipAddress
-     *    The IP address of the record to delete. May be
-     *    null.
-     *
-     * @return
-     *    TRUE if the record could be deleted, FALSE otherwise.
-     */
-    public static boolean deleteRecord(String name, IPAddress ipAddress){
-
-	boolean deleted = false;
-	boolean match = false;
-
-	Record deleteMe = null;
-	String address = null;
-
-	String addressTemp = null;
-
-	    // Go through all the records
-	for (Record temp : records){
-
-	    match = false;
-
-		// Try to match against the supplied name
-	    match = temp.getName().equals(name);
-
-		// If the supplied IP address isn't null, check
-		// against it as well
-	    if (ipAddress.getIPAddress() != null){
-		address = ipAddress.getIPAddress();
-		addressTemp = temp.getIPAddress().getIPAddress();
-		match = match && addressTemp.matches(address);
-	    }
-
-		// Also check against supplied port number, if it
-		// isn't == 0
-	    if (ipAddress.getPort() > 0){
-		match = match && (temp.getIPAddress().getPort()
-					 == ipAddress.getPort());
-	    }
-
-		// If found, break from loop
-	    if (match){
-		deleteMe = temp;
-		break;
-	    }
-
-	} // end foreach
-
-	    // If a record was found, attempt to delete it
-	if (deleteMe != null){
-	    deleted = records.remove(deleteMe);
-	}
-
-	return deleted;
-
-    } // end method deleteRecord
-
-
-    public static Record getRecord(String name){
-
-	Record record = null;
-
-	for (Record temp : records){
-	    if (temp.getName().equals(name)){
-		record = temp;
-		break;
-	    }
-	}
-
-	return record;
-
-    }
-
-    /**
-     * Find records that match the given regular expressions.
-     *
-     * @param name
-     *    The regular expression against which to compare
-     *    the name field of records.
      * @param address
-     *    The regular expression against which to compare
-     *    the IP address field of records.
-     *
-     * @return
-     *    A LinkedList of Record objects that match the given
-     *    regular expressions.
+     *    An IPAddress to which the message is sent.
+     * @param message
+     *    The message to send to the IPAddress.
      */
-    public static LinkedList<Record> findRecords(String name, String address){
+    public static void sendMail(IPAddress address, String message){
 
-	    // Parse the given regex for the address
-	String addressRegex = IPAddress.parseIPAddressRegex(address);
-	IPAddress ipRegex = new IPAddress(addressRegex);
+	message = message.replace("\n.\n","");
+	ClientProtocol.send(message, address);
 
-	    // Create empty list in which to put matched records
-	LinkedList<Record> matchedRecords = new LinkedList<Record>();
-
-	    // Run checks against all records in the list
-	for (Record temp : records){
-
-	    if (temp.matches(name, ipRegex)){
-		matchedRecords.addLast(temp);
-	    } // end if temp.matches
-
-	} // end foreach
-
-	return matchedRecords;
-
-    } // end method findRecords
+    } // end sendMail    
 
 } // end class Server
